661cef25b3f89d584fe333f8ae1e9215ac7b2dc2
Sam Byron <mrc4090@proton.me>
2024-02-04 13:47:06 +0200

    Correctly run model evaluation. Need significant cleanup. Garbage collection as implemented may be significantly deteriorating time performance

--- translation.py
+++ translation.py
@@ -9,6 +9,10 @@ import torch.nn as nn
 from torchinfo import summary
 import math
 import time
+import os
+
+# https://pytorch.org/docs/stable/notes/cuda.html
+# os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:1024"
 
 # open file
 with open('./spa-eng/spa.txt', 'r') as fp:
@@ -104,18 +108,6 @@ class RNN(nn.Module):
                            batch_first=True)
         self.decoder_rnn = nn.LSTM(target_embed_dim, rnn_hidden_size, batch_first=True)
         self.fc = nn.Linear(rnn_hidden_size, target_vocab_size)
-
-    # def forward(self, src_lang, target_lang, d_hidden, d_cell, generate=False):
-    #     src_embedding = self.embedding(src_lang)
-    #     out, (e_hidden, e_cell) = self.encoder_rnn(src_embedding)
-    #     target_embedding = self.target_embedding(target_lang)
-    #     # out, (hidden, cell) = self.decoder_rnn(target_embedding, (torch.squeeze(hidden,0), torch.squeeze(cell,0)))
-    #     if generate:
-    #         out, (d_hidden, d_cell) = self.decoder_rnn(target_embedding, (d_hidden, d_cell))
-    #     else:
-    #         out, (d_hidden, d_cell) = self.decoder_rnn(target_embedding, (e_hidden, e_cell))
-    #     out = self.fc(out)
-    #     return out, d_hidden, d_cell
     
 
     def forward(self, src_lang, target_lang, d_hidden, d_cell):
@@ -153,6 +145,8 @@ summary(model, input_data=[src_batch_summary, target_batch_summary, hidden, cell
 loss_fn = nn.CrossEntropyLoss(reduction='mean')
 
 # Train
+from statistics import mean
+import gc
 num_epochs = 15
 
 def train_procedure(dataloader, model, optimizer, device):
@@ -174,12 +168,67 @@ def train_procedure(dataloader, model, optimizer, device):
         loss.backward()
         optimizer.step()
         total_loss += loss
+        max_test_predictions = reordered_pred.argmax(axis=1)
+        mislabeled = torch.ne(max_test_predictions, target_batch[:,1:])
+        acc = 0
+        crnt_batch_size = len(mislabeled[:,0])
+        batch_acc = []
+        for i in range(crnt_batch_size):
+            acc = [1 if x == False else 0 for x in mislabeled[i,:]]
+            acc = mean(acc)
+            batch_acc.append(acc)
+        total_acc += mean(batch_acc)
         counter += 1
-    return total_loss/counter
+        del src_batch
+        del target_batch
+        del pred
+        del hidden
+        del cell
+        del reordered_pred
+        gc.collect()
+        torch.cuda.empty_cache()
+    return total_loss/counter, total_acc/counter
 
 
-TRAIN = False
-LOAD = True
+def validation_procedure(dataloader, model, device):
+    model.eval()
+    # model.to(device)
+    # model.to("cpu")
+    with torch.no_grad():
+        total_acc, total_loss = 0, 0
+        counter = 0
+        for src_batch, target_batch in dataloader:
+            src_batch = src_batch.to(device)
+            target_batch = target_batch.to(device)
+            hidden, cell = model.init_hidden(batch_size)
+            pred, hidden, cell = model(src_batch, target_batch[:,:-1], hidden, cell)
+            reordered_pred = pred.permute(0,2,1)
+            loss = loss_fn(reordered_pred, target_batch[:,1:])
+            total_loss += loss
+            max_test_predictions = reordered_pred.argmax(axis=1)
+            mislabeled = torch.ne(max_test_predictions, target_batch[:,1:])
+            acc = 0
+            crnt_batch_size = len(mislabeled[:,0])
+            batch_acc = []
+            for i in range(crnt_batch_size):
+                acc = [1 if x == False else 0 for x in mislabeled[i,:]]
+                acc = mean(acc)
+                batch_acc.append(acc)
+            total_acc += mean(batch_acc)
+            counter += 1
+            del src_batch
+            del target_batch
+            del pred
+            del hidden
+            del cell
+            del reordered_pred
+            gc.collect()
+            torch.cuda.empty_cache()
+    return total_loss/counter, total_acc/counter
+
+
+TRAIN = True
+LOAD = False
 SAVE = False
 PATH = './models/en_spa_translation/en_spa_translation.pt'
 if LOAD:
@@ -193,12 +242,12 @@ if TRAIN:
     start_time = time.time()
     device = torch.device("cuda:0")
     for epoch in range(num_epochs):
-        loss = train_procedure(train_dl, model, optimizer, device)
-        val_loss = train_procedure(valid_dl, model, optimizer, device)
+        loss, acc = train_procedure(train_dl, model, optimizer, device)
+        val_loss, val_acc = validation_procedure(valid_dl, model, device)
     
         if epoch % 1 == 0:
             end_time = time.time()
-            print(f'Epoch {epoch} loss: {loss:.4f} val_loss: {val_loss:.4f} time: {(end_time - start_time):.3f} secs')
+            print(f'Epoch {epoch} loss: {loss:.4f} acc {acc:0.4f} val_loss: {val_loss:.4f} val_acc: {val_acc:0.4f} time: {(end_time - start_time):.3f} secs')
             start_time = time.time()
             # Save learned model
             if SAVE:
@@ -210,33 +259,6 @@ from torch.distributions.categorical import Categorical
                 
 model.eval()
 
-# def sample(model, en_sentence, scale_factor=1.0):
-    
-#     model.to('cpu')
-
-#     source_text_transform = lambda src: [en_vocab[token] for token in tokenizer(src)]
-#     en_sentence = torch.tensor(source_text_transform(en_sentence))
-
-#     next_word = start_translation_token
-#     target_text_transform = lambda target: [spa_vocab[token] for token in tokenizer(target)]
-#     next_word = torch.tensor(target_text_transform(next_word))
-
-#     seq_length = 10
-#     spa_sentence = ""
-#     hidden, cell = model.init_hidden(1)
-#     end_trans_token_vec = torch.tensor(target_text_transform(end_translation_token))
-#     counter = 0
-#     while next_word != end_trans_token_vec and counter < 10:
-#         translation_logits, hidden, cell = model(en_sentence, next_word, hidden, cell, generate=True)
-#         translation_logits = torch.nn.functional.softmax(translation_logits, dim=1)
-#         m = Categorical(logits=translation_logits)
-#         next_word = m.sample()
-#         next_word = spa_vocab.lookup_token(next_word)
-#         spa_sentence += next_word + " "
-#         next_word = torch.tensor(target_text_transform(next_word))
-#         counter += 1
-    
-#     return spa_sentence
 
 def sample(model, en_sentence, scale_factor=1.0):
     
@@ -270,5 +292,6 @@ def sample(model, en_sentence, scale_factor=1.0):
     return spa_sentence
 
 print(sample(model, "How are you?"))
-# print(sample(model, "What time is it"))
-# print(sample(model, "I want to go home"))
+print(sample(model, "What time is it"))
+print(sample(model, "I want to go home"))
+print(sample(model, "Good night"))
